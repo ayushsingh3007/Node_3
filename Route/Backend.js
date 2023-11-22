cda9892ee57abbfa22f7e884e18793988f8c8d45
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-11-22 06:09:55 -0800

    Delete Route directory

--- Route/Backend.js
+++
@@ -1,6 +0,0 @@
-const backend=require('express').Router()
-
-backend.get("/",(req,res)=>{
-    return res.send("backend data is there ")
-})
-module.exports=backend;
--- Route/UserDetail.js
+++
@@ -1,9 +0,0 @@
-const routes=require('express').Router();
-
-routes.post("/",(req,res)=>{
-   return  res.send("Registered user ")
-})
-routes.post("/login",(req,res)=>{
-    return res.send("login successfully")
-})
-module.exports=routes;
